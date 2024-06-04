8e87ceb69eae8130e62bc7e9d2381a60fd1af50f
Roni Milstein <milsteinroni@gmail.com>
2024-06-04 20:17:12 +0300

    added tools lines and context. added some fragments, changing names to camelCase for clarity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -60,6 +61,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.material)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/plantcruiser/MainMenuFragment.kt
@@ -0,0 +1,27 @@
+package com.example.plantcruiser
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.plantcruiser.databinding.DbMenuFragmentBinding
+import com.example.plantcruiser.databinding.MainMenuFragmentBinding
+
+
+class MainMenuFragment : Fragment() {
+    private var _binding : MainMenuFragmentBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MainMenuFragmentBinding.inflate(inflater,container,false)
+
+        binding.databaseButton
+
+        findNavController().navigate(R.id.action_mainMenuFragment_to_DBFragment)
+    }
+    return super.onCreateView(inflater, container, savedInstanceState)
+}
