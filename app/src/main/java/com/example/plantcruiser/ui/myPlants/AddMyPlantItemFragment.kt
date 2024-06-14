32877f17819763677b4758726a7b875be2dfc7fc
Roni Milstein <milsteinroni@gmail.com>
2024-06-14 12:37:36 +0300

    modify some properties of edit text, made size of card consistent, add toast for attempt to add empty plant

--- app/src/main/java/com/example/plantcruiser/ui/myPlants/AddMyPlantItemFragment.kt
+++ app/src/main/java/com/example/plantcruiser/ui/myPlants/AddMyPlantItemFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -44,17 +45,22 @@ class AddMyPlantItemFragment : Fragment(){
 
         binding.finishButton.setOnClickListener {
             val plantingDate = binding.plantingDateText.text.toString()
-            val newPlant = MyPlant(
-                id = 0, // Assuming auto-generate, so setting to 0 or not at all if the ID is not required here.
-                name = binding.plantNameText.text.toString(),
-                image = null,
-                plantingDate = plantingDate,
-                sunlight = binding.plantSunlightText.text.toString(),
-                watering = binding.plantWateringText.text.toString(),
-                fertilizingFreq = binding.plantFertilizingFrequencyText.text.toString(),
-                disease = binding.plantDiseaseText.text.toString())
-            viewModel.insert(newPlant)
-            findNavController().navigate(R.id.action_addMyPlantItemFragment_to_myPlantsFragment)
+            if (binding.plantNameText.text.toString().isNotEmpty()) {
+                val newPlant = MyPlant(
+                    id = 0, // Assuming auto-generate, so setting to 0 or not at all if the ID is not required here.
+                    name = binding.plantNameText.text.toString(),
+                    image = null,
+                    plantingDate = plantingDate,
+                    sunlight = binding.plantSunlightText.text.toString(),
+                    watering = binding.plantWateringText.text.toString(),
+                    fertilizingFreq = binding.plantFertilizingFrequencyText.text.toString(),
+                    disease = binding.plantDiseaseText.text.toString())
+                viewModel.insert(newPlant)
+                findNavController().navigate(R.id.action_addMyPlantItemFragment_to_myPlantsFragment)
+            } else {
+                Toast.makeText(requireContext(), getString(R.string.empty_plant_msg), Toast.LENGTH_SHORT).show()
+            }
+
 
 
         }
